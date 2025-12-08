dee2d55d1677ffdd9e396c4e4b34198df5be23ed
alexschoefer <alex.schoefer@gmx.net>
2025-12-08 21:44:23 +0100

    feat: add some material design; add two new components dashboard and customer; add some new routes for the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent},
+    { path: 'customer', component: CustomerComponent}
+];
