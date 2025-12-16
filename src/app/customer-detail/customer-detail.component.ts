0fca5235a3f61670ba3f31a96c46ecfa9242c0e2
alexschoefer <alex.schoefer@gmx.net>
2025-12-16 21:48:00 +0100

    feat: working on the customer-detail component; add firebase data for each id

--- src/app/customer-detail/customer-detail.component.ts
+++ src/app/customer-detail/customer-detail.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-detail',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './customer-detail.component.html',
   styleUrl: './customer-detail.component.scss'
 })
-export class CustomerDetailComponent {
+export class CustomerDetailComponent implements OnInit {
 
+  customer$!: Observable<any>;
+  firestore = inject(Firestore);
+
+  constructor(private route: ActivatedRoute) {}
+
+  //wird aufgerufen, wenn Componente initalisiert wird
+  ngOnInit(): void {
+    //holt die aktuelle ID aus der URL Route
+    const id = this.route.snapshot.paramMap.get('id')!;
+    //sucht genau die Daten, wo die ID passt
+    const customerRef = doc(this.firestore, 'customers', id);
+    //holt die Daten als Observerable (docData = liest das Dokument, liefert ein Observerable, gibt nur den Inhalt zurück)
+    this.customer$ = docData(customerRef, { idField: 'id' });
+  }
 }
